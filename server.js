496215001d5fe35fd5a91780139c1dbecf998e08
Denzale Reese <dlr249@cornell.edu>
2016-09-16 00:34:23 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function homepage(req, res){
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.listen(3000, function() {
+  console.log("Server listening at localhost:3000");
+});
