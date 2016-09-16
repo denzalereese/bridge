30d6e1dcc48975ff2f59808569bed676472a8d0a
Denzale Reese <dlr249@cornell.edu>
2016-09-16 00:36:50 -0700

    ready for deploy

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', function homepage(req, res){
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server listening at localhost:3000");
 });
